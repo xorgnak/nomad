38037d0df1fe014b49e42eb45bc02b007eb8b262
E. Leonard <xorgnak@gmail.com>
2021-08-25 19:13:01 -0400

    no docs

--- nomadic/exe/nomad.sh
+++ nomadic/exe/nomad.sh
@@ -20,7 +20,7 @@ apt-get -qq update
 apt-get -y -qq install $SCRIPT_PACKAGES $DISTRO_PACKAGES
 echo -e "$X GEMS"
 
-gem install $DISTRO_GEMS 2>1 /dev/null
+#gem install $DISTRO_GEMS 2>1 /dev/null
 gem install --no-rdoc --no-ri $DISTRO_GEMS 2>1 /dev/null
 
 echo -e "$X SCREEN"
