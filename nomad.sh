b9900c5359823b884e348049786d26781ffe814c
Erik Olson <xorgnak@gmail.com>
2021-10-21 02:22:18 -0600

    stuff.

--- nomad.sh
+++ nomad.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
+DEBS='git screen ruby-full redis-server redis-tools build-essential certbot nginx emacs-nox mosquitto';
+GEMS='sinatra thin eventmachine slop redis-objects pry rufus-scheduler twilio-ruby';
+
+
 mkdir -p run
 mkdir -p nginx
 if [[ "$1" == "config" ]]; then
-rm run.sh
-cat <<EOF > run/$DOMAIN.sh;
+    rm run.sh
+    cat ~/nomad.conf > run/$DOMAIN.sh
+cat <<EOF >> run/$DOMAIN.sh;
 #
 # EDIT TO SUIT YOUR NEEDS.
 #
 export DOMAIN_ROOT='$DOMAIN_ROOT';
 export DOMAIN='$DOMAIN';
 export PORT='$PORT';
-export PHONE_SID='$PHONE_SID';
-export PHONE_TOKEN='$PHONE_KEY';
 export PHONE='$PHONE';
 export ADMIN='$ADMIN';
 export FREQUENCY='$FREQUENCY';
+
 ruby nomad-coin.rb -p \$PORT -d \$DOMAIN -b \$ADMIN -f \$FREQUENCY &
 EOF
 emacs run/$DOMAIN.sh;
@@ -35,14 +39,36 @@ server {
 }
 EOF
 
+sudo cp nginx/* /etc/nginx/sites-enabled/
+sudo service nginx restart
+
 elif [[ "$1" == "install" ]]; then
-    sudo apt update && sudo apt upgrade && sudo apt install git screen ruby-full redis-server redis-tools build-essential certbot nginx emacs-nox;
-    sudo gem install sinatra thin eventmachine slop redis-objects pry rufus-scheduler twilio-ruby;
+    if [[ ! -f ~/nomad.conf ]]; then
+    cat <<EOF > ~/nomad.conf
+#
+# do no edit.
+#
+# network wide configuration.
+#
+export DOMAIN_ROOT='$DOMAIN_ROOT'; 
+export PHONE_SID='$PHONE_SID';
+export PHONE_TOKEN='$PHONE_KEY';
+#
+# end of network configuration.
+#
+EOF
+    nano ~/nomad.conf
+    fi
+    sudo apt update && sudo apt upgrade && sudo apt install $DEBS;
+    sudo gem install $GEMS;
+    sudo cp nginx.conf /etc/nginx/
+    sudo service nginx restart
 elif [[ "$1" == "commit" ]]; then
     git add . && git commit && git push;
 elif [[ "$1" == "quick" ]]; then
     ./nomad.sh install && ./nomad.sh config && ./nomad.sh $*
 else
+    git pull
     for f in run/*.sh;
     do
 	./$f
